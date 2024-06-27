eee6558721089c40898dd570b6adf194ee0c5114
panukorn17 <p.taleo17@gmail.com>
2024-06-27 17:18:24 +0100

    update to include meshing

--- freeCAD/CAD_reconstruction/src/main.py
+++ freeCAD/CAD_reconstruction/src/main.py
@@ -3,11 +3,16 @@ import sys
 sys.path.append('C:/Users/ASUS/UCL/Research Assistant/code/OPSIDIAN/freeCAD/CAD_reconstruction/src')
 
 from utils.face_generation import calculate_face_coords, generate_faces, add_spine, sweep
+from utils.cfd_utils import create_analysis_container
+from utils.mesh_utils import mesh
 
 if __name__ == "__main__":
-    baseline_factor = [0.8, 0.3]
+    baseline_factor = [1, 1]
+    base_element_size = 50.8 #mm
     faces_coordinate = calculate_face_coords(baseline_factor)
     face_shapes = generate_faces(faces_coordinate)
     spine = add_spine()
-    sweep(face_shapes, spine)
+    sweep = sweep(face_shapes, spine)
+    create_analysis_container()
+    mesh(sweep, base_element_size)
     #exec(open('C:/Users/ASUS/UCL/Research Assistant/code/OPSIDIAN/freeCAD/CAD_reconstruction/src/main.py').read())

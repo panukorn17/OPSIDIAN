6845416bb5a3f81024240c47ab3e2ca8c3cf44e4
panukorn17 <p.taleo17@gmail.com>
2024-07-16 14:14:40 +0100

    update cfd_utils

--- src/utils/cfd_utils.py
+++ src/utils/cfd_utils.py
@@ -18,3 +18,29 @@ def create_analysis_container():
     analysis.addObject(CfdFluidMaterial.makeCfdFluidMaterial('FluidProperties'))
     analysis.addObject(CfdInitialiseFlowField.makeCfdInitialFlowField())
     analysis.addObject(CfdSolverFoam.makeCfdSolverFoam())
+
+def setup_physics_model(model, time='Steady', turbulence='Laminar', turbulenceModel='kOmegaSST'):
+    """
+    Function to set up the physics model for CfdOF
+    """
+    model.Time = time
+    model.Turbulence = turbulence 
+    model.TurbulenceModel = turbulenceModel
+
+def setup_fluid_properties(model_fluid_properties, name='Water', type='Isothermal', density='998 kg/m^3', dynamicViscosity='1.003e-3 kg/m/s'):
+    """
+    Function to set up the fluid properties for CfdOF
+    """
+    if name == 'Water':
+        desc = 'Standard distilled water properties at 20 Degrees Celsius and 1 atm'
+        model_fluid_properties.Label = 'Water'
+    else:
+        desc = ''
+        model_fluid_properties.Label = 'Fluid'
+    model_fluid_properties.Material = {'CardName': name + type,
+                                       'AuthorAndLicence': '',
+                                       'Name': name,
+                                       'Type': type,
+                                       'Description': desc,
+                                       'Density': density,
+                                       'DynamicViscosity': dynamicViscosity}
